ff6851f12af9d6f9097a94c66f065ca1baa699b9
Saw Simon Linn <simonlinn2@gmail.com>
2025-08-21 21:49:35 -0700

    localstorage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Toaster } from '@/components/ui/toaster';
-import { cn } from '@/lib/utils';
-import { AppProvider } from '@/context/app-context';
-import { SidebarProvider } from '@/components/ui/sidebar';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
+import { cn } from "@/lib/utils";
+import { AppProvider } from "@/context/app-context";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
-  title: 'BizBoost',
-  description: 'Advanced Franchise Fee Calculator + Franchise Performance Dashboard',
+  title: "BizBoost",
+  description:
+    "Advanced Franchise Fee Calculator + Franchise Performance Dashboard",
 };
 
 export default function RootLayout({
@@ -19,13 +21,21 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
       </head>
-      <body className={cn('font-body antialiased bg-background')}>
+      <body className={cn("font-body antialiased bg-background")}>
         <AppProvider>
           <SidebarProvider>
             {children}
+            <Analytics />
           </SidebarProvider>
         </AppProvider>
         <Toaster />
